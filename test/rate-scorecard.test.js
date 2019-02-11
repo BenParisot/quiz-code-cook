d8fc120d53f556e95deb1e3319a41e6ad792ba41
Ben Parisot <me@benparisot.com>
2019-02-11 13:42:02 -0800

    add other tests to rate scorecard function

--- test/rate-scorecard.test.js
+++ test/rate-scorecard.test.js
@@ -10,12 +10,35 @@ function rateScorecard(scorecard) {
     if(dwarf > elf && dwarf > human) {
         return 'Dwarf';
     }
+
+    else if(elf > dwarf && elf > human) {
+        return 'Elf';
+    }
+    else {
+        return 'Human';
+    }
 }
 
 test('return "Dwarf" if dwarf outscores other characters', function(assert) {
     const scorecard = { dwarf: 1, elf: 0, human: 0 };
     const result = rateScorecard(scorecard);
     const expected = 'Dwarf';
-    assert.deepEqual(result, expected);
+    assert.equal(result, expected);
+    
+});
+
+test('return "Elf" if dwarf outscores other characters', function(assert) {
+    const scorecard = { dwarf: 0, elf: 1, human: 0 };
+    const result = rateScorecard(scorecard);
+    const expected = 'Elf';
+    assert.equal(result, expected);
+    
+});
+
+test('return "Human" if dwarf outscores other characters', function(assert) {
+    const scorecard = { dwarf: 0, elf: 0, human: 1 };
+    const result = rateScorecard(scorecard);
+    const expected = 'Human';
+    assert.equal(result, expected);
     
 });
